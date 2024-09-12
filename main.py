e53e08526fa597afb64c0d1ce16433e9c51a37ca
abdullahjawaid30 <abdullahjawaid30@gmail.com>
2024-09-12 17:05:04 +0500

    Creating A Common Interface

--- main.py
+++ main.py
@@ -22,57 +22,50 @@ items = [
 ]
 
 # get-items URL
-# http://127.0.0.1:5000/get-items
-@app.route('/get-items', methods=['GET'])
+@app.route('/items', methods=['GET'])
 def get_items():
     return {"items": items}
 
 # get-item individual URL
-# http://127.0.0.1:5000/get-item
-@app.route('/get-item', methods=['GET'])
+@app.route('/item', methods=['GET'])
 def get_item():
     name = request.args.get('name')
-    print(name)
     for item in items:
         if name == item['name']:
             return item
-        
-    return {"message": "Item does not exist"},404   
+    return {"message": "Item does not exist"}, 404
 
 # add-item URL
-# http://127.0.0.1:5000/add-item
-@app.route('/add-item', methods=['POST'])
+@app.route('/item', methods=['POST'])
 def add_item():
     request_data = request.get_json()
+    # Check if the item already exists
+    for item in items:
+        if item['name'] == request_data['name']:
+            return {"message": "Item already exists"}, 400
+
     items.append(request_data)
     return {"message": "Item added successfully"}, 201
 
-
-# update URL
-# http://127.0.0.1:5000/update-item
-@app.route('/update-item', methods=['PUT'])
+# update-item URL
+@app.route('/item', methods=['PUT'])
 def update_item():
     request_data = request.get_json()
     for item in items:
-        if item['name']== request_data['name']:
-            item['price']=request_data['price']
+        if item['name'] == request_data['name']:
+            item['price'] = request_data['price']
             return {"message": "Item updated successfully"}
-        
-    return {"message": "IGiven Record Does Not Exixts"},404
+    return {"message": "Given record does not exist"}, 404
 
-
-# delete-item individual URL
-# http://127.0.0.1:5000/delete-item
-@app.route('/delete-item', methods=['DELETE'])
+# delete-item URL
+@app.route('/item', methods=['DELETE'])
 def delete_item():
     name = request.args.get('name')
-    print(name)
     for item in items:
         if name == item['name']:
             items.remove(item)
-            return {"message": "Item delete successfully"}
-        
-    return {"message": "Item does not exist"},404   
+            return {"message": "Item deleted successfully"}
+    return {"message": "Item does not exist"}, 404
 
 if __name__ == "__main__":
     app.run()
